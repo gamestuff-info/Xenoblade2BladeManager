37e6866f309b3bd49f4375dba2035ae2924bf881
Dan Keenan <dk@dankeenan.org>
2018-01-22 18:41:27 -0500

    Test the access controls (Owned validator and BladeVoter)

---
+++ tests/Security/Voter/BladeVoterTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Tests\Security\Voter;
+
+use App\Entity\Blade;
+use App\Entity\Role;
+use App\Entity\User;
+use App\Security\Voter\BladeVoter;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BladeVoterTest extends TestCase
+{
+
+    /**
+     * @var AccessDecisionManagerInterface
+     */
+    protected $decisionManager;
+
+    /**
+     * @var UserInterface
+     */
+    protected $currentUser;
+
+    /**
+     * @var UserInterface
+     */
+    protected $otherUser;
+
+    /**
+     * @var TokenInterface
+     */
+    protected $token;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $roleUser = (new Role())->setName('ROLE_USER');
+        $this->currentUser = (new User())
+          ->setUsername('User 1')
+          ->addRole($roleUser);
+        $this->otherUser = (new User())
+          ->setUsername('User 2')
+          ->addRole($roleUser);
+        $tokenMock = self::createMock(TokenInterface::class);
+        $tokenMock->method('getUser')->willReturn($this->currentUser);
+        $this->token = $tokenMock;
+
+        $decisionManager = self::createMock(AccessDecisionManagerInterface::class);
+        $this->decisionManager = $decisionManager;
+    }
+
+    /**
+     * Test access checking on Blades.
+     *
+     * @dataProvider bladeDataProvider
+     *
+     * @param $attribute
+     * @param $entity
+     * @param $result
+     */
+    public function testVote($attribute, $entity, $result)
+    {
+        $bladeVoter = new BladeVoter($this->decisionManager);
+        self::assertEquals($result, $bladeVoter->vote($this->token, $entity, [$attribute]));
+    }
+
+    public function bladeDataProvider()
+    {
+        $roleUser = (new Role())->setName('ROLE_USER');
+        $this->currentUser = (new User())
+          ->setUsername('User 1')
+          ->addRole($roleUser);
+        $this->otherUser = (new User())
+          ->setUsername('User 2')
+          ->addRole($roleUser);
+        $ownedBlade = (new Blade())
+          ->setUser($this->currentUser);
+        $otherBlade = (new Blade())
+          ->setUser($this->otherUser);
+
+        return [
+          'edit/allowed' => [
+            'EDIT',
+            $ownedBlade,
+            VoterInterface::ACCESS_GRANTED,
+          ],
+          'edit/disallowed' => [
+            'EDIT',
+            $otherBlade,
+            VoterInterface::ACCESS_DENIED,
+          ],
+          'delete/allowed' => [
+            'DELETE',
+            $ownedBlade,
+            VoterInterface::ACCESS_GRANTED,
+          ],
+          'delete/disallowed' => [
+            'DELETE',
+            $otherBlade,
+            VoterInterface::ACCESS_DENIED,
+          ],
+          'view/allowed' => [
+            'VIEW',
+            $ownedBlade,
+            VoterInterface::ACCESS_GRANTED,
+          ],
+          'view/disallowed' => [
+            'VIEW',
+            $otherBlade,
+            VoterInterface::ACCESS_DENIED,
+          ],
+        ];
+    }
+}
---
+++ tests/Validator/Constraints/OwnedValidatorTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\OwnedInterface;
+use App\Entity\User;
+use App\Validator\Constraints\Owned;
+use App\Validator\Constraints\OwnedValidator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
+
+class OwnedValidatorTest extends TestCase
+{
+
+    /**
+     * @var UserInterface
+     */
+    protected $currentUser;
+
+    /**
+     * @var UserInterface
+     */
+    protected $otherUser;
+
+    /**
+     * @var TokenStorageInterface
+     */
+    protected $tokenStorage;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->currentUser = (new User())->setUsername('User 1');
+        $this->otherUser = (new User())->setUsername('User 2');
+
+        $tokenMock = self::createMock(TokenInterface::class);
+        $tokenMock->method('getUser')->willReturn($this->currentUser);
+        $tokenStorageMock = self::createMock(TokenStorageInterface::class);
+        $tokenStorageMock->method('getToken')->willReturn($tokenMock);
+        $this->tokenStorage = $tokenStorageMock;
+    }
+
+    /**
+     * Test an owned entity is valid
+     */
+    public function testValidateValid()
+    {
+        $entity = self::createMock(OwnedInterface::class);
+        $entity->method('getUser')->willReturn($this->currentUser);
+        $context = self::createMock(ExecutionContextInterface::class);
+        $context->expects(self::never())
+          ->method('buildViolation');
+        $constraint = new Owned();
+
+        $validator = new OwnedValidator($this->tokenStorage);
+        $validator->initialize($context);
+        $validator->validate($entity, $constraint);
+    }
+
+    /**
+     * Test an unowned entity is invalid
+     */
+    public function testValidateInvalid()
+    {
+        $entity = self::createMock(OwnedInterface::class);
+        $entity->method('getUser')->willReturn($this->otherUser);
+        $context = self::createMock(ExecutionContextInterface::class);
+        $violationBuilder = self::createMock(ConstraintViolationBuilderInterface::class);
+        $violationBuilder->method('setParameter')
+          ->willReturnSelf();
+        $context->expects(self::once())
+          ->method('buildViolation')
+          ->willReturn($violationBuilder);
+        $constraint = new Owned();
+
+        $validator = new OwnedValidator($this->tokenStorage);
+        $validator->initialize($context);
+        $validator->validate($entity, $constraint);
+    }
+}
