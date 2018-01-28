6318619727d8469d8c971120e5c9b9c965676158
Dan Keenan <dk@dankeenan.org>
2018-01-28 11:15:14 -0500

    Revise test fixtures to edit merc missions

--- fixtures/tests/MercMissionControllerTest/testEdit.php
+++ fixtures/tests/MercMissionControllerTest/testEdit.php
@@ -8,6 +8,9 @@ return [
       'prerequisites' => '<numberBetween(0, 1)>x @merc_mission_prerequisite_entity_*',
       'duration' => '<dateTimeBetween("@0", "@5400")>',
       'repeatable' => '<(true)>',
+      'mercPoints' => '<numberBetween(50, 150)>',
+      'experience' => '<numberBetween(0, 1000)>',
+      'gold' => '<numberBetween(0, 2000)>',
     ],
   ],
 ];
