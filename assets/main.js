5009a349d33dad2d4bb46b1c613f139efc9f1f2f
Dan Keenan <dk@dankeenan.org>
2018-06-06 10:51:28 -0400

    Upgrade dependencies

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,8 @@
+import './scss/xeno2.scss';
+
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import '@fortawesome/fontawesome';
 import '@fortawesome/fontawesome/styles.css';
 import 'tablesorter/dist/css/theme.bootstrap_4.min.css';
-
-import './scss/xeno2.scss';
