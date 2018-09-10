749613d9c9c7ad335b2b21cfcd09683d844d973b
Dan Keenan <dk@dankeenan.org>
2018-09-09 20:36:04 -0400

    Update dependencies

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,6 @@ import './scss/xeno2.scss';
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
-import '@fortawesome/fontawesome';
-import '@fortawesome/fontawesome/styles.css';
+import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import 'tablesorter/dist/css/theme.bootstrap_4.min.css';
