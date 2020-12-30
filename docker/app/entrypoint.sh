28d5e8be2ca6bd899bc654bed235a32010149a35
Dan Keenan <dk@dankeenan.org>
2020-12-30 15:09:25 -0500

    Prepare for DigitalOcean App Platform

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,4 +4,4 @@ chown -R www-data:www-data var
 php -d memory_limit=256M bin/console cache:clear
 bin/console assets:install
 
-php-fpm
+apache2-foreground
--- docker/web/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Apply configuration
-envsubst '${NGINX_BACKEND_HOST}' \
-    < /etc/nginx/conf.d/default.conf.tmpl > /etc/nginx/conf.d/default.conf
-
-nginx -g "daemon off;"
